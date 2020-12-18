58f314add94d074b63cb89f27820762af3f3000a
Edwin Pineda <61611378+edwinpineda96@users.noreply.github.com>
2020-12-18 14:48:06 -0600

    Add files via upload

--- consulta_siedu.sql
+++ consulta_siedu.sql
@@ -41,31 +41,31 @@ Select mpob.id,
 	mpob.ind_dist_m,
 	mpob.shape_le_1,
 	mpob.shape_area,
-	m1.total_long as distedui,
-	m1.estandar_c as est_est_edui,
-	m3.total_long as distedub,
-	m3.estandar_c as est_edub,
-	m7.total_long as distsalud,
-	m7.estandar_c as est_salud,
-	m20.total_long as distplaza,
-	m20.estandar_c as est_plaza,
-	m22.total_long as distparque,
-	m22.estandar_c as est_parque,
-	m25.total_long as distparadero,
-	m25.estandar_c as est_paradero
+	m1.total_long As distedui,
+	m1.estandar_c As est_est_edui,
+	m3.total_long As distedub,
+	m3.estandar_c As est_edub,
+	m7.total_long As distsalud,
+	m7.estandar_c As est_salud,
+	m20.total_long As distplaza,
+	m20.estandar_c As est_plaza,
+	m22.total_long As distparque,
+	m22.estandar_c As est_parque,
+	m25.total_long As distparadero,
+	m25.estandar_c As est_paradero
 	
-From "SIEDU_BPU3_Manzana_Por_Pob" mpob Inner Join "SIEDU_BPU1_Manzana_Dist_Ind" m1
-On mpob.manzent::varchar = m1.manzent_t::varchar
+From "SIEDU_BPU3_Manzana_Por_Pob" mpob Join "SIEDU_BPU1_Manzana_Dist_Ind" m1
+On mpob.manzent_t::varchar = m1.manzent_t::varchar
 Join "SIEDU_BPU3_Manzana_Dist_Ind" m3
-On mpob.manzent::varchar = m3.manzent_t::varchar
+On mpob.manzent_t::varchar = m3.manzent_t::varchar
 Join "SIEDU_BPU7_Manzana_Dist_Ind" m7
-On mpob.manzent::varchar = m7.manzent_t::varchar
+On mpob.manzent_t::varchar = m7.manzent_t::varchar
 Join "SIEDU_BPU20_Manzana_Dist_Ind" m20
-On mpob.manzent::varchar = m20.manzent_t::varchar
+On mpob.manzent_t::varchar = m20.manzent_t::varchar
 Join "SIEDU_BPU1_BPU22_Manzana_Dist_Ind" m22
-On mpob.manzent::varchar = m22.manzent_t::varchar
+On mpob.manzent_t::varchar = m22.manzent_t::varchar
 Join "SIEDU_BPU25_Manzana_Dist_Ind" m25
-On mpob.manzent::varchar = m25.manzent_t::varchar
+On mpob.manzent_t::varchar = m25.manzent_t::varchar
 )
 
-Select * From "Join_SIEDU_DIST_MANZANAS" limit 6
+-- Select * From "Join_SIEDU_TEST_4"
